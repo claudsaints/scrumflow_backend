bbd7e1a94d80fe0e7e56b5c0b3b6e6e6c9a12e10
claudsaints <claudiodsantoss7@gmail.com>
2025-07-23 21:57:39 -0300

    feat: CORS configuration

--- src/main/java/com/claudsaints/scrumflow/security/config/SecurityConfiguration.java
+++ src/main/java/com/claudsaints/scrumflow/security/config/SecurityConfiguration.java
@@ -15,6 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -81,5 +86,6 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 
 }
